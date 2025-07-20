4ea02c434cdb0aff6a19782ab527bba8e2c66f4b
jhuang007007 <jhuang007007@gmail.com>
2025-07-20 14:28:15 -0400

    add details component, dynamic router for details

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@
  *  Protractor is used in this example for compatibility with Angular documentation tools.
  */
 import {bootstrapApplication, provideProtractorTestingSupport} from '@angular/platform-browser';
+import {provideRouter} from '@angular/router';
+import routeConfig from './app/routes';
 import {App} from './app/app';
 
-bootstrapApplication(App, {providers: [provideProtractorTestingSupport()]}).catch((err) =>
-  console.error(err),
-);
+bootstrapApplication(App, {
+  providers: [provideProtractorTestingSupport(), provideRouter(routeConfig)],
+}).catch((err) => console.error(err));
